518158c079010ecab24762c7d8a38c4ad8372ee5
Erik Dubois <erik.dubois@gmail.com>
2018-02-24 11:12:10 +0100

    first commit

---
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+#giving tmp folder extra gb in order not to run out of disk space while installing software
+#only if you run into issues with that
+#sudo mount -o remount,size=5G,noatime /tmp
+
+echo "DESKTOP SPECIFIC APPLICATIONS"
+
+echo "Installing category Accessories"
+
+sudo pacman -S --noconfirm --needed gnome-multi-writer
+sudo pacman -S --noconfirm --needed gnome-pie
+sudo pacman -S --noconfirm --needed galculator
+
+echo "Installing category Development"
+
+#sudo pacman -S --noconfirm --needed gnome-boxes
+
+echo "Installing category Education"
+
+echo "Installing category Games"
+
+echo "Installing category Graphics"
+
+echo "Installing category Internet"
+
+echo "Installing category Multimedia"
+
+sudo pacman -S --noconfirm --needed gnome-mplayer
+sh AUR/install-mpd-ncmpcpp-v*.sh
+
+echo "Installing category Office"
+
+echo "Installing category Other"
+
+echo "Installing category System"
+
+sudo pacman -S --noconfirm --needed nautilus-image-converter
+sudo pacman -S --noconfirm --needed dmenu
+#variety
+sudo pacman -S --noconfirm --needed libappindicator-gtk3
+echo "################################################################"
+echo "####    Software from Arch Linux Repository installed     ######"
+echo "################################################################"
