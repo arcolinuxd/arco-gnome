8f4e11a8e93ed225d25026a3d0c9160d26292b06
Erik Dubois <erik.dubois@gmail.com>
2018-09-05 14:56:46 +0200

    config specific

--- 600-software-from-ArcoLinux-repo-v1.sh
+++ 600-software-from-ArcoLinux-repo-v1.sh
@@ -25,8 +25,8 @@ sudo pacman -S arcolinux-bin-git --noconfirm --needed
 #sudo pacman -S arcolinux-bspwm-git --noconfirm --needed
 #sudo pacman -S arcolinux-common-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-cinnamon-git --noconfirm --needed
-sudo pacman -S arcolinux-config-git --noconfirm --needed
-#sudo pacman -S arcolinux-config-gnome-git --noconfirm --needed
+#sudo pacman -S arcolinux-config-git --noconfirm --needed
+sudo pacman -S arcolinux-config-gnome-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-mate-git --noconfirm --needed
 #sudo pacman -S arcolinux-config-plasma-git --noconfirm --needed
 sudo pacman -S arcolinux-conky-collection-git --noconfirm --needed
